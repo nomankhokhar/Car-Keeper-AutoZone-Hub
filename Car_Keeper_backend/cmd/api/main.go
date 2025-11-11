b4231b6bd10f5d1c5ddae858dd4da1ded93f9832
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2025-11-12 00:20:45 +0500

    Change port name description

--- Car_Keeper_backend/cmd/api/main.go
+++ Car_Keeper_backend/cmd/api/main.go
@@ -110,7 +110,7 @@ func main() {
 		port = "8080"
 	}
 
-	log.Printf("Server starting on port %s", port)
+	log.Printf("Server is Started on port %s", port)
 	if err := router.Run(":" + port); err != nil {
 		log.Fatal("Failed to start server:", err)
 	}
